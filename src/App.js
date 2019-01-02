237eecd2b817deb7ecdcf58bcf5d45bd950d690c
jeh26033 <jeh26033@email.vccs.edu>
2019-01-02 13:03:26 -0500

    Update App.js
    nav

--- src/App.js
+++ src/App.js
@@ -10,13 +10,30 @@ import Index from './components/index.component';
 class App extends Component {
   render() {
     return (
-
-      <div className = "row">
-        <div className="col-md text-center">
-          <h2>React CRUD Tutorial</h2>
-        </div>
-        <div className="col-md text-center">
-          <h2>React CRUD Tutorial</h2>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to={'/'} className="navbar-brand">Whiskey Gaming</Link>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                  <Link to={'/'} className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/create'} className="nav-link">Create</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={'/index'} className="nav-link">Index</Link>
+                </li>
+              </ul>
+            </div>
+          </nav> <br/>
+          <h2>Welcome to the Whiskey Gaming site</h2> <br/>
+          <Switch>
+              <Route exact path='/create' component={ Create } />
+              <Route path='/edit/:id' component={ Edit } />
+              <Route path='/index' component={ Index } />
+          </Switch>
         </div>
       </Router>
     );
