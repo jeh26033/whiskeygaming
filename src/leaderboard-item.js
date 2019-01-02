c7cbf3c6f26ace7a77bde99f599772a456cb3c6b
Mike Carmody <michael.carmody@asurion.com>
2019-01-02 15:37:16 -0500

    colors!

--- src/leaderboard-item.js
+++ src/leaderboard-item.js
@@ -6,22 +6,27 @@ export default class Navbar extends Component {
       render() {
             return (
                   <div className = "leaderboard-item">
-                  	<div className = "hero-info">
-                  		<img src = "/img/rubick_test.png" className = "hero-icon"></img>
-                  		<div className = "hero-name">Rubick</div>
+                  	<div className = "player-info">
+                  		<img src = "/img/rubick_test.png" className = "hero-icon" />
+                              <div className = "name-block">
+                        		<div className = "hero-name">Rubick</div>
+                                    <div className = "player-name">WSKB.Dark Arbiter</div>
+                              </div>
                   	</div>
                   	<div className = "hero-statline">
-                  		<div className = "hero-kda">3 / 2 / 14</div>
+                              <div className = "hero-level">19</div>
+                  		<div className = "hero-kda"><span className = "hero-kills">3</span> / <span className = "hero-deaths">2</span> / <span className = "hero-assists">14</span></div>
+                              <div className = "hero-cs"><span className = "hero-lh">104</span>/<span className = "hero-denies">10</span></div>
                   		<div className = "hero-gpm">322 gpm</div>
                   		<div className = "hero-xpm">450 xpm</div>
                   	</div>
                   	<div className = "hero-items-list">
-                  		<img src = "test-item.png" className = "statline-item"></img>
-                  		<img src = "test-item.png" className = "statline-item"></img>
-                  		<img src = "test-item.png" className = "statline-item"></img>
-                  		<img src = "test-item.png" className = "statline-item"></img>
-                  		<img src = "test-item.png" className = "statline-item"></img>
-                  		<img src = "test-item.png" className = "statline-item"></img>
+                  		<img src = "test-item.png" className = "statline-item" />
+                  		<img src = "test-item.png" className = "statline-item" />
+                  		<img src = "test-item.png" className = "statline-item" />
+                  		<img src = "test-item.png" className = "statline-item" />
+                  		<img src = "test-item.png" className = "statline-item" />
+                  		<img src = "test-item.png" className = "statline-item" />
                   	</div>
                   </div>
             )
