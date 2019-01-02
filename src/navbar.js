278e1ea022798df243ad5a52faa90cb05c7d7108
Mike Carmody <michael.carmody@asurion.com>
2019-01-02 12:56:01 -0500

    add nav elements

--- src/navbar.js
+++ src/navbar.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import './navbar.css';
 
 export default class Navbar extends Component {
 
-	constructor() {
-		super();
-	}
 
 	render() {
     	return (
-        	<div class="nav-container"></div>
+        	<div className = "nav-container">
+        		<div className = "nav-link" id = "home-link">HOME</div>
+        		<div className = "nav-link" id = "leaderboard-link">LEADERBOARD</div>
+        		<div className = "nav-link" id = "profile-link">PROFILE</div>
+	        	<div className = "button" id = "hamburger-icon">&#9776;</div>
+        		<div className = "button" id = "steam-login-button">LOGIN WITH STEAM</div>
+    		</div>
 		)
   }
 }	
