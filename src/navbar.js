b22c3cbbdb39a54ad6db1599f0176d8221c04443
Mike Carmody <michael.carmody@asurion.com>
2019-01-02 12:19:25 -0500

    initial commit

---
+++ src/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+
+	constructor() {
+		super();
+	}
+
+	render() {
+    	return (
+        	<div class="nav-container"></div>
+		)
+  }
+}	
