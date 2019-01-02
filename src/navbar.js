d788aff5a05624987d150e9050b52a83608976dc
Mike Carmody <michael.carmody@asurion.com>
2019-01-02 13:12:23 -0500

    replace profile with join

--- src/navbar.js
+++ src/navbar.js
@@ -9,9 +9,8 @@ export default class Navbar extends Component {
         	<div className = "nav-container">
         		<div className = "nav-link" id = "home-link">HOME</div>
         		<div className = "nav-link" id = "leaderboard-link">LEADERBOARD</div>
-        		<div className = "nav-link" id = "profile-link">PROFILE</div>
 	        	<div className = "button" id = "hamburger-icon">&#9776;</div>
-        		<div className = "button" id = "steam-login-button">LOGIN WITH STEAM</div>
+        		<div className = "nav-link" id = "join-link">JOIN</div>
     		</div>
 		)
   }
