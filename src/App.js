a93e1cf8ec5c3e6b6039a25af0ace2b1c5892b76
jeh26033 <jeh26033@email.vccs.edu>
2019-01-02 13:03:09 -0500

    beginning nav
    probably won't be used.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@
 
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
+import Create from './components/create.component';
+import Edit from './components/edit.component';
+import Index from './components/index.component';
 class App extends Component {
   render() {
     return (
@@ -14,7 +18,7 @@ class App extends Component {
         <div className="col-md text-center">
           <h2>React CRUD Tutorial</h2>
         </div>
-      </div>
+      </Router>
     );
   }
 }
