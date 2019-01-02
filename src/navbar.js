e39bbfe3b084959fa4ed606adc838ec417509dce
Mike Carmody <michael.carmody@asurion.com>
2019-01-02 13:16:54 -0500

    updated with weird link formatting

--- src/navbar.js
+++ src/navbar.js
@@ -7,10 +7,16 @@ export default class Navbar extends Component {
 	render() {
     	return (
         	<div className = "nav-container">
-        		<div className = "nav-link" id = "home-link">HOME</div>
-        		<div className = "nav-link" id = "leaderboard-link">LEADERBOARD</div>
+        		<div className = "nav-link" id = "home-link">
+                    <Link to={'/'}>HOME</Link>
+                </div>
+        		<div className = "nav-link" id = "leaderboard-link">
+                    <Link to={'/'}>LEADERBOARD</Link>
+                </div>
 	        	<div className = "button" id = "hamburger-icon">&#9776;</div>
-        		<div className = "nav-link" id = "join-link">JOIN</div>
+        		<div className = "nav-link" id = "join-link">
+                    <Link to={'/'}>JOIN</Link>
+                </div>
     		</div>
 		)
   }
