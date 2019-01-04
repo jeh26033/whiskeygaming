2cf2e787c41203f4d276e355b1d54bc1abf12e0d
Mike Carmody <michael.carmody@asurion.com>
2019-01-04 15:28:54 -0500

    Revert "Revert "cors fuckery""
    This reverts commit ccaa1e1ad7de70f4a8af87c29328b1b0a8577792.

--- src/leaderboard.js
+++ src/leaderboard.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import './leaderboard.scss';
 import LeaderboardItem from './leaderboard-item';
 
-export default class Navbar extends Component {
+const api_key = "f4903c56-7589-4d13-9a36-6a8fac44f2d1";
+const openDotaURL = "https://api.opendota.com/api/matches/4311550282?api_key=" + api_key;
+
+export default class Leaderboard extends Component {
+
+      constructor() {
+            super();
+
+            fetch(openDotaURL, {
+                  method: "GET", // *GET, POST, PUT, DELETE, etc.
+                  mode: "cors", // no-cors, cors, *same-origin
+                  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+                  headers: {
+                      "Content-Type": "application/json; charset=utf-8",
+                      // "Content-Type": "application/x-www-form-urlencoded",
+                  },
+                  redirect: "follow", // manual, *follow, error
+                  referrer: "no-referrer" // no-referrer, *client
+            });
+      };
 
 
       render() {
