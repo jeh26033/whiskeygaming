395c2fd56a74389c2feeca9ea67a3c78528e13c5
Mike Carmody <michael.carmody@asurion.com>
2019-01-10 11:35:27 -0500

    basic non-database functioning of adding players

--- src/joinMenu.js
+++ src/joinMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './joinMenu.scss';
+import './joinmenu.scss';
 import playerIDList from './playerIDList.json';
 
 export default class JoinMenu extends Component {
@@ -10,13 +10,27 @@ export default class JoinMenu extends Component {
             this.state = {
                   
             }
+
+            this.handleSubmit = this.handleSubmit.bind(this);
+            console.log(playerIDList);
+      }
+
+      handleSubmit(event) {
+            var input = this;
+            var content = document.querySelectorAll('.join-ID-field')[0].value;
+            playerIDList.push(parseInt(content));
+            console.log(playerIDList);
       }
 
       render() {
 
             return (
                   <div className = "join-menu-container">
-                       
+                        <div className = "join-input-container">
+                                    <input className = "join-ID-field"></input>
+                                    <button className = "ID-submit-button" onClick = {this.handleSubmit} >Submit</button>
+                              
+                        </div>
                   </div>
             )
       }
