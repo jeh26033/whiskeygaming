f2e1c8faba05f6a38dda48f072143e4b35e33b49
Mike Carmody <michael.carmody@asurion.com>
2019-01-17 19:22:22 -0500

    styles!

--- src/latestGames.js
+++ src/latestGames.js
@@ -28,6 +28,14 @@ export default class LatestGames extends Component {
 
             return (
                   <div className = "latest-game-container">
+                        <div className = "latest-games-header">
+                              <div className = "latest-games-header-item" id = "first-header-item">Role</div>
+                              <div className = "latest-games-header-item">Level</div>
+                              <div className = "latest-games-header-item">KDA</div>
+                              <div className = "latest-games-header-item">Lh/Dn</div>
+                              <div className = "latest-games-header-item" id = "gpm-xpm">GPM/XPM</div>
+                              <div className = "latest-games-header-item" id = "net-worth">Net<br/>Worth</div>
+                        </div>
                         {this.state.latestGamesHTML}
                   </div>
             )
